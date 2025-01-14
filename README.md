0646d219d8d98626077574fb94a1081fb71b9e6b
andreschaparro <chaparroandres87@gmail.com>
2025-01-14 09:02:23 -0300

    Mongo con Docker está funcionando

---
+++ README.md
@@ -0,0 +1,19 @@
+# Proyecto: Plataforma IoT para una Raspberry Pi 3 B+
+
+## Etapa 1: Base de datos Mongo
+
+Comandos útiles:
+
+- `docker compose down`.
+- `sudo rm -rf mongo/datadir`.
+- `docker compose up --build -d`.
+
+Prueba con MongoDB Compass:
+
+!["url"](./imagenes/1-url.png).
+
+!["db"](./imagenes/2-db.png).
+
+## Etapa2: API para la base de datos
+
+TODO
---
+++ mongo/mongo-init.js
@@ -0,0 +1,28 @@
+// Crea el usuario para que la api se conecte a la base de datos
+db.createUser({
+    user: "iotuser",
+    pwd: "iot123",
+    roles: [
+        {
+            role: "readWrite",
+            db: "iot"
+        }
+    ]
+})
+
+// Crea las colecciones de la base de datos
+db.createCollection("iotAcciones")
+db.createCollection("iotAtributos")
+db.createCollection("iotDatos")
+db.createCollection("iotDispositivos")
+db.createCollection("iotGrupos")
+db.createCollection("iotUsuarios")
+
+// Crea el usuario admin por defecto de la plataforma
+db.iotUsuarios.insertOne({
+    name: "admin",
+    email: "admin@admin.com",
+    // El password "admin123" se debe cifrar en https://bcrypt-generator.com/ utilizando 10 rounds
+    password: "$2a$10$qa1ZnRuxovoH.NUgOuwKZeC63pZvjilPNHx8hniuvAVKoqShazr32",
+    rol: "admin"
+})

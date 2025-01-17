60f23ae23d58da5a08bd1fec78382dd5e6c91d5f
andreschaparro <chaparroandres87@gmail.com>
2025-01-17 10:33:26 -0300

    La api ya funciona con Docker

--- api/config/database.config.js
+++ api/config/database.config.js
@@ -1,10 +1,11 @@
 const {
+    MONGO_HOSTNAME = "172.16.36.141",
     MONGO_USERNAME = "iotuser",
     MONGO_PASSWORD = "iot123",
     MONGO_DATABASE = "iot"
 } = process.env
 
-export const MONGO_URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@172.16.36.141:27017/${MONGO_DATABASE}`
+export const MONGO_URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DATABASE}`
 
 // Cantidad máxima de telemetrías que se guardan por device durante un día
 export const MAX_SAMPLES = 24 * 60 * 60
